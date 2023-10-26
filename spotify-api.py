0a600f004c544447366a2332ed9ca9a6a8610d58
Chasen Brutocao <chasenb1@gmail.com>
2023-10-26 16:23:39 -0500

    Update spotify-api.py

--- spotify-api.py
+++ spotify-api.py
@@ -1,166 +1,84 @@
 import os
 import spotipy
 import pandas as pd
-from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
-# Assign Spotify API credentials
+# Define Spotify API credentials
 client_id = 'XXXXXXXXXX'
 client_secret = 'XXXXXXXXXXX'
-redirect_uri='http://localhost:3000'
-scope='user-read-recently-played'
+redirect_uri = 'http://localhost:3000'
+scopes = ['user-read-recently-played', 'user-top-read', 'user-library-read']
 
-# Authenticate with the Spotify API
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_uri,
-                                               scope=scope))
+# Function to authenticate with Spotify API
+def authenticate_with_spotify(scope):
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
 
-# Create a function to handle empty URIs
+# Authenticate with the Spotify API for each scope
+sp_recent = authenticate_with_spotify('user-read-recently-played')
+sp_top = authenticate_with_spotify('user-top-read')
+sp_saved = authenticate_with_spotify('user-library-read')
+
+# Define a function to handle empty URIs
 def error_handler_uri(argument):
-    if argument is None:
-        return 'N/A'
-    else:
-        return argument
+    return argument if argument is not None else 'N/A'
 
-### DATASET 1  - Recently Listened Songs ###
+# Function to extract recent songs data
+def extract_recent_songs_data():
+    recent_songs = sp_recent.current_user_recently_played(limit=25)
+    data = []
+    for item in recent_songs['items']:
+        data.append({
+            'Song': item['track']['name'],
+            'Song Duration': item['track']['duration_ms'],
+            'Time Played At': item['played_at'],
+            'Artist': item['track']['artists'][0]['name'],
+            'Artist Popularity Score': item['track']['popularity'],
+            'Album': item['track']['album']['name'],
+            'Album Type': item['track']['album']['album_type'],
+            'Album Release Date': item['track']['album']['release_date'],
+            'Total Tracks on Album': item['track']['album']['total_tracks'],
+            'Album Cover': item['track']['album']['images'][0]['url'],
+            'URI': error_handler_uri(item['context']['uri'])
+        })
+    return pd.DataFrame(data)
 
-# Define which data I am pulling from the API
-recent_songs = sp.current_user_recently_played(limit=25)
+# Function to extract my top artists data
+def extract_top_artists_data():
+    ranges = ['short_term', 'medium_term', 'long_term']
+    data = {range: [] for range in ranges}
+    for sp_range in ranges:
+        results = sp_top.current_user_top_artists(time_range=sp_range, limit=25)
+        for item in results['items']:
+            data[sp_range].append({'Artist': item['name'], 'Genres': ', '.join(item['genres'])})
+    return pd.DataFrame(data)
 
-# Create empty lists to hold different data
-twenty_recent_songs = []
-song_duration = [] 
-twenty_recent_artists = [] 
-artist_popularity_score = [] 
-time_played_at=[] 
-album = [] 
-album_type = [] 
-album_image = [] 
-album_release_date = [] 
-total_tracks_on_album = [] 
-uri = [] 
+# Function to extract my recent saved tracks data
+def extract_saved_tracks_data():
+    user_saved_tracks = sp_saved.current_user_saved_tracks(limit=50)
+    data = []
+    for item in user_saved_tracks['items']:
+        data.append({
+            'Song': item['track']['name'],
+            'Song Duration': item['track']['duration_ms'],
+            'Time Added At': item['added_at'],
+            'Artist': item['track']['artists'][0]['name'],
+            'Artist Popularity Score': item['track']['popularity'],
+            'Album': item['track']['album']['name'],
+            'Album Type': item['track']['album']['album_type'],
+            'Album Release Date': item['track']['album']['release_date'],
+            'Total Tracks on Album': item['track']['album']['total_tracks'],
+            'Album Cover': item['track']['album']['images'][0]['url'],
+            'URI': error_handler_uri(item['track']['uri'])
+        })
+    return pd.DataFrame(data)
 
-# Use a for loop to iterate through the enumerated object and append values to the appropiate list
-for key, value in enumerate(recent_songs['items']):
-    twenty_recent_songs.append(value['track']['name'])
-    twenty_recent_artists.append(value['track']['artists'][0]['name'])
-    album.append(value['track']['album']['name'])
-    time_played_at.append(value['played_at'])
-    song_duration.append(value['track']['duration_ms'])
-    artist_popularity_score.append(value['track']['popularity'])
-    album_type.append(value['track']['album']['album_type'])
-    album_image.append(value['track']['album']['images'][0]['url'])
-    album_release_date.append(value['track']['album']['release_date'])
-    total_tracks_on_album.append(value['track']['album']['total_tracks'])
-    if error_handler_uri(recent_songs['items'][key]['context'])=='N/A':
-        uri.append('No URI Available')
-    else:
-        uri.append(value['context']['uri'])
-
-# Declare a Pandas dataframe to create a table with data
-recent_songs_df = pd.DataFrame(list(zip(twenty_recent_songs, song_duration, time_played_at, twenty_recent_artists, 
-                           artist_popularity_score, album, album_type, album_release_date, total_tracks_on_album, 
-                           album_image, uri)),
-                  columns=['Song', 'Song Duration', 'Time Played At', 'Artist', 'Artist Popularity Score', 
-                           'Album', 'Album Type', 'Album Release Date', 'Total Tracks on Album', 'Album Cover',
-                           'URI'])
-
-### DATASET 2 - Favorite Artists Short/Medium/Long Term ###
-
-# Define a new data scope and listening history ranges
-scope = 'user-top-read'
-ranges = ['short_term', 'medium_term', 'long_term']
-
-# Ensure credentials are correct
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_uri, scope=scope))
-# Create empty lists to hold different data
-short_term_list=[]
-medium_term_list=[]
-long_term_list=[]
-short_term_genre=[]
-medium_term_genre=[]
-long_term_genre=[]
-
-# Use a nested for loop to iterate through and append results to appropiate list
-for sp_range in ranges:
-    #print("range:", sp_range)
-    results = sp.current_user_top_artists(time_range=sp_range, limit=25)
-
-    for i, item in enumerate(results['items']):
-        if sp_range=='short_term':
-            short_term_list.append(item['name'])
-            short_term_genre.append(", ".join(item['genres']))
-        if sp_range=='medium_term':
-            medium_term_list.append(item['name'])
-            medium_term_genre.append(", ".join(item['genres']))
-        if sp_range=='long_term':
-            long_term_list.append(item['name'])
-            long_term_genre.append(", ".join(item['genres']))
-
-# Declare a Pandas dataframe to create dataset
-time_period_trends_df = pd.DataFrame(list(zip(short_term_list, short_term_genre, medium_term_list, medium_term_genre, long_term_list, long_term_genre)),columns=['Short_Term', 'Short_Term_Genre', 'Medium_Term', 'Medium_Term_Genre', 'Long_Term', 'Long_Term_Genre'])
-
-# Pivot data
-pivoted_table = time_period_trends_df.melt(var_name=['Time Period'])
-
-# Rename columns
-pivoted_table.rename(columns={"value": "Arist"})
-
-### DATASET 3 - Recently Saved Songs ###
-
-# Define the scope
-scope = 'user-library-read'
-
-# Ensure credentials are correct
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_uri, scope=scope))
-# Define which data I am pulling from the API
-user_saved_tracks = sp.current_user_saved_tracks(limit=50)
-
-# Define empty lists
-saved_tracks = []
-song_duration = [] 
-time_added_at = []
-artists = [] 
-artist_popularity_score = [] 
-album = [] 
-album_type = [] 
-album_image = [] 
-album_release_date = [] 
-total_tracks_on_album = [] 
-uri = [] 
-
-# Iterate through the enumerated object and append to appropiate list
-for key, value in enumerate(user_saved_tracks['items']):
-    saved_tracks.append(value['track']['name'])
-    artists.append(value['track']['artists'][0]['name'])
-    time_added_at.append(value['added_at'])
-    album.append(value['track']['album']['name'])
-    song_duration.append(value['track']['duration_ms'])
-    artist_popularity_score.append(value['track']['popularity'])
-    album_type.append(value['track']['album']['album_type'])
-    album_image.append(value['track']['album']['images'][0]['url'])
-    album_release_date.append(value['track']['album']['release_date'])
-    total_tracks_on_album.append(value['track']['album']['total_tracks'])
-    if error_handler_uri(user_saved_tracks['items'][key]['track']['uri'])=='N/A':
-        uri.append('No URI Available')
-    else:
-        uri.append(value['track']['uri'])
-
-# Declare a Pandas dataframe to create a dataset
-recent_saved_tracks_df = pd.DataFrame(list(zip(saved_tracks, song_duration, time_added_at, artists, 
-                           artist_popularity_score, album, album_type, album_release_date, total_tracks_on_album, 
-                           album_image, uri)),
-                  columns=['Song', 'Song Duration', 'Time Added At', 'Artist', 'Artist Popularity Score', 
-                           'Album', 'Album Type', 'Album Release Date', 'Total Tracks on Album', 'Album Cover',
-                           'URI'])
+# Run each function
+recent_songs_df = extract_recent_songs_data()
+time_period_trends_df = extract_top_artists_data()
+recent_saved_tracks_df = extract_saved_tracks_data()
 
 # Use Pandas function ExcelWriter to save all 3 dataframes to 1 Excel file
 with pd.ExcelWriter('Spotify_API_Data.xlsx') as writer:
-    recent_songs_df.to_excel(writer, sheet_name='Recent Songs')
-    time_period_trends_df.to_excel(writer, sheet_name='Time Period Trends')
-    recent_saved_tracks_df.to_excel(writer, sheet_name='Liked Songs')
+    recent_songs_df.to_excel(writer, sheet_name='Recent Songs', index=False)
+    time_period_trends_df.to_excel(writer, sheet_name='Time Period Trends', index=False)
+    recent_saved_tracks_df.to_excel(writer, sheet_name='Liked Songs', index=False)
